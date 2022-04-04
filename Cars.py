d9a4cf3b8f3064215207b09cedefc6af25969d65
Jcrimm <jicrimm@syr.edu>
2022-04-04 16:57:52 -0400

    Playing with API

--- Cars.py
+++ Cars.py
@@ -10,13 +10,33 @@ Created on Sat Apr  2 17:06:43 2022
 import pandas as pd
 import requests
 
+#%% Trial import censsus tracts of interest
+boston = pd.read_csv("census-tract-data.csv",dtype=str)
+#keep all rows except first one
+boston = boston.iloc[1:]
+tracts = boston["TRACT"].to_list()
+tract_string = ",".join(tracts)
+
+
+#%%
 # Trial import
 api = 'https://api.census.gov/data/2020/acs/acs5'
-#Select all counties
-for_clause = 'county:*'
-#no in clause because looking at entire country
+#Variables of interest
+#"NAME"
+#""B01001_001E": total population
+#"B01001A_001E": white alone
+#"B01001B_001E" black alone
+#"B01001D_001E" asian alone
+#"B01001G_001E" two or more races
+#"B01001I_002E" hispanic alone
+#"B06011_001E" median income in the last 12 months
+#"B25113_001E" median gross rent by when person moved into unit
+#Select Massachusetts
+for_clause = tract_string
+#Select tracts of interest
+in_clause = "state:25"
 key_value = "0a95ea1ddf62885731b2000925bbf002a1a803c2"
-payload = {'get':"NAME,B01001_001E,B08201_001E", 'for':for_clause,'key':key_value}
+payload = {'get':"NAME,B01001_001E,B08201_001E", 'for':for_clause,'in':in_clause,'key':key_value}
 response = requests.get(api,payload)
 
 #check whether it worked
