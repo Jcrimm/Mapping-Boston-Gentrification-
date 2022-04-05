79e00371a98074a562052e5bfa7de03acce2af7f
Jcrimm <jicrimm@syr.edu>
2022-04-05 09:33:15 -0400

    Updated script

--- 2020.py
+++ 2020.py
@@ -10,15 +10,25 @@ Created on Sat Apr  2 17:06:43 2022
 import pandas as pd
 import requests
 
-#%% Trial import censsus tracts of interest
-boston = pd.read_csv("census-tract-data.csv",dtype=str)
-#keep all rows except first one
+#%% Create a string that has all census tracts of interest
+
+#Import "census-tract-data.csv", which has basic information on all the censsu tracts in Boston. 
+# For now, let's just use the GEOID, GEOCODE, STATE, COUNTY, and TRACT columns.
+#use argument dtype=str to import them as strings
+boston = pd.read_csv("census-tract-data.csv",usecols=['GEOID','GEOCODE','STATE','COUNTY','TRACT'], dtype=str)
+
+#keep all rows except first one, which has explainers of each variable
 boston = boston.iloc[1:]
+
+#Write all the tracts to a list
 tracts = boston["TRACT"].to_list()
+
+#Join the list into one string seperated by commas.
+#this will be handy for when we make the API requests
 tract_string = ",".join(tracts)
 
+#%% Create API request for 2020 ACS 5-year estimate
 
-#%%
 # Trial import
 api = 'https://api.census.gov/data/2020/acs/acs5'
 #Variables of interest
@@ -31,10 +41,12 @@ api = 'https://api.census.gov/data/2020/acs/acs5'
 #"B01001I_002E" hispanic alone
 #"B06011_001E" median income in the last 12 months
 #"B25113_001E" median gross rent by when person moved into unit
-#Select Massachusetts
-for_clause = tract_string
-#Select tracts of interest
-in_clause = "state:25"
+
+#Select tracts of interest--only those in Boston! 
+for_clause = {"tract":tract_string}
+
+#For the in clause, want Massachusetts and Suffolk County
+in_clause = "state:25 County:025"
 key_value = "0a95ea1ddf62885731b2000925bbf002a1a803c2"
 payload = {'get':"NAME,B01001_001E,B08201_001E", 'for':for_clause,'in':in_clause,'key':key_value}
 response = requests.get(api,payload)
