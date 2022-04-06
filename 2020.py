e387c0b066485faf2735e80997b5ad794dbfc042
Jcrimm <jicrimm@syr.edu>
2022-04-06 11:03:59 -0400

    Updated script to use 2010 census tracts. Hashed out variables of interest

--- 2020.py
+++ 2020.py
@@ -10,42 +10,65 @@ Created on Sat Apr  2 17:06:43 2022
 import pandas as pd
 import requests
 
-#%% Create a string that has all census tracts of interest
+#%% Create a string that has all census tracts of interest (2020 tracts)
 
-#Import "census-tract-data.csv", which has basic information on all the censsu tracts in Boston. 
+#Import "census-tract-data.csv", which has basic information on all the censsu tracts in Boston in 2020. 
 # For now, let's just use the GEOID, GEOCODE, STATE, COUNTY, and TRACT columns.
 #use argument dtype=str to import them as strings
-boston = pd.read_csv("census-tract-data.csv",usecols=['GEOID','GEOCODE','STATE','COUNTY','TRACT'], dtype=str)
+boston2020 = pd.read_csv("census-tract-data.csv",usecols=['GEOID','GEOCODE','STATE','COUNTY','TRACT'], dtype=str)
 
 #keep all rows except first one, which has explainers of each variable
-boston = boston.iloc[1:]
+boston2020 = boston2020.iloc[1:]
 
 #Write all the tracts to a list
-tracts = boston["TRACT"].to_list()
+tracts2020 = boston2020["TRACT"].to_list()
 
 #Join the list into one string seperated by commas.
 #this will be handy for when we make the API requests
-tract_string = ",".join(tracts)
+tract_string2020 = ",".join(tracts2020)
+
+#%% Create a string that has all census tracts of interest (2010 tracts)
+
+# Import "Census_2010_Tracts.csv" has basic information on all the censsu tracts in Boston in 2010
+boston2010 = pd.read_csv("Census_2010_Tracts.csv",dtype=str)
+
+#Write all the tracts to a list
+tracts2010 = boston2010["TRACTCE10"].to_list()
+
+#Join the list into one string seperated by commas
+#this will be handy for when we make the API requests
+tract_string2010 = ",".join(tracts2010)
+
 
 #%% Bring in Variables of interest
-table = pd.read_csv("ACS2020_Table_Shells.csv",dtype=str)
-table = table.rename(columns={"Table ID":"table_ID","Unique ID":"unique_ID"})
+
 #Variables of interest (Table Shells)
 #"NAME"
 #""B01001_001E": total population
-#"B02001": Race (doesn't include Hispanic/Latino)
-#"B03002": Hispanic origin
-#"B19001" median income in the last 12 months
-#"B15003" educational attainment for population over 25
-#B25026 tenure for occupied units
-#"B25063" gross rent
-pop = table.query("unique_ID =='B01001_001'")
-race = table.query("table_ID == 'B02001'")
-hispanic = table.query("table_ID == 'B03002'")
-median_income = table.query("table_ID == 'B19001'")
+#"B03002_002" : Not Hispanic or Latino: White Alone
+#"B19013_001" median income in the last 12 months, inflation adjusted
+#"B15003" educational attainment for population over 25 (table ID)
+#"B25064_001": Median Rent
+
+#Create a list of all the variables we want, exluding education.
+var_list = ["NAME", "B01001_001E","B03002_002","B19013_001","B25064_001"]
+
+# For education, want the full table. Let's select all the variables using the table shell csv file
+table = pd.read_csv("ACS2020_Table_Shells.csv",dtype=str)
+#rename columns so they don't have spaces
+table = table.rename(columns={"Table ID":"table_ID","Unique ID":"unique_ID"})
+#select rows for educational attainment for populaton over 25
 education = table.query("table_ID == 'B15003'")
-tenure = table.query("table_ID == 'B25026'")
-rent = table.query("table_ID == 'B25063'")
+#drop first two rows
+education = education.iloc[2:]
+#write the remaining variables to a list
+attainment = education["unique_ID"].to_list()
+
+#Join the education list onto var_list
+var_list.extend(attainment)
+
+#create a string for all the variables
+var_string = ",".join(var_list)
 
 
 #%% Create API request for 2020 ACS 5-year estimate
@@ -55,12 +78,13 @@ rent = table.query("table_ID == 'B25063'")
 api = 'https://api.census.gov/data/2020/acs/acs5'
 
 #Select tracts of interest--only those in Boston! 
-for_clause = {"tract":tract_string}
+for_clause = {"tract":tract_string2010}
 
 #For the in clause, want Massachusetts and Suffolk County
 in_clause = "state:25 County:025"
+#put in census API key. 
 key_value = "0a95ea1ddf62885731b2000925bbf002a1a803c2"
-payload = {'get':"NAME,B01001_001E,B08201_001E", 'for':for_clause,'in':in_clause,'key':key_value}
+payload = {'get': var_string, 'for':for_clause,'in':in_clause,'key':key_value}
 response = requests.get(api,payload)
 
 #check whether it worked
@@ -82,17 +106,15 @@ colnames = row_list[0]
 datarows = row_list[1:]
 
 #Convert the data into a Pandas dataframe
-cars_2020 = pd.DataFrame(columns=colnames, data=datarows)
+data2020 = pd.DataFrame(columns=colnames, data=datarows)
 
 #rename population and cars
-cars_2020 = cars_2020.rename(columns={"B01001_001E":"Population","B08201_001E":"Cars"})
+
 
 #convert population and cars into integers
-cars_2020["Population"] = cars_2020["Population"].astype(float)
-cars_2020["Cars"] = cars_2020["Cars"].astype(float)
 
 #create cars per population
-cars_2020["cars_per_person"] = cars_2020["Cars"]/cars_2020["Population"]
+
 
 
 
